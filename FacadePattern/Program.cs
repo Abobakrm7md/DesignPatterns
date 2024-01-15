353dd953bfc0dc3f983b515872a838af37f96f81
AboBakr Mohamed <abobakr.mohamed@itworxedu.com>
2024-01-15 17:01:13 +0200

    ..

--- FacadePattern/Program.cs
+++ FacadePattern/Program.cs
@@ -1,4 +1,7 @@
-﻿using FacadePattern.FacadeInterface;
+﻿//The Facade design pattern provides a unified interface to a set of interfaces in a subsystem.
+//This pattern defines a higher-level interface that makes the subsystem easier to use.
+
+using FacadePattern.FacadeInterface;
 using FacadePattern.FacadeSubSystem;
 
 PerchaseOrder order = new PerchaseOrder(new StockAvailability(), new PlaceOrder(), new Invoice(), new PaymentService(), new EmailService());
